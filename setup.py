77f2bb9d473838c4659a808a65a3dd9660c11556
Shaohua Xu <127070418+xsh93@users.noreply.github.com>
2025-07-08 13:45:40 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='btstn',
+    name='toc_btstn',
     version='1.0',
-    description='Bidirectional Time-series State Transfer Network (BTSTN)',
-    author='Shaohua Xu, Ting Xu, Yuping Yang, Xin Chen',
+    description='Bidirectional Time-series State Transfer Network for Target-directed Adptive Control of Metabolic Processes',
+    author='Shaohua Xu, Xin Chen',
     author_email='12218022@zju.edu.cn',
     packages=find_packages(),
     install_requires=[
-        'torch==2.0.0',
-        'numpy>=1.23.5',
-        'pandas>=1.5.2',
-        'pypots==0.5'
+        'torch==2.7.0',
+        'numpy==2.0.2',
+        'pandas==2.2.3',
+        'scikit-learn==1.6.1'
     ]
 )
